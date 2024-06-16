eec0e7f63bef529378a34d14bbe41ed826a31215
aig77 <29128253+aig77@users.noreply.github.com>
2024-06-16 19:39:09 -0400

    Add swww

---
+++ .config/swww/scripts/swww_change_wallpaper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $# -lt 1 ]] || [[ ! -d $1 ]]; then
+    echo "Usage:
+    $0 <dir containing images>"
+    exit 1
+fi
+
+#export SWWW_TRANSITION_FPS=60
+#export SWWW_TRANSITION_STEP=2
+#export SWWW_TRANSITION=random
+WAL_START=2
+
+img=$(find $1 -type f -name "*.jpg" -o -name "*.png" -o -name "*.gif" | shuf -n 1)
+
+swww img "$img" 
+cp "$img" ~/.cache/current_wallpaper
+#cp "$img" ~/.config/rofi/background
+sleep $WAL_START
+~/.config/wal/wal-set.sh "$img"
+
+exit 1
---
+++ .config/swww/scripts/swww_randomize.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# This script will randomly go through the files of a directory, setting it
+# up as the wallpaper at regular intervals
+#
+# NOTE: this script is in bash (not posix shell), because the RANDOM variable
+# we use is not defined in posix
+
+if [[ $# -lt 1 ]] || [[ ! -d $1 ]]; then
+	echo "Usage:
+	$0 <dir containing images> <interval length in minutes>
+    negative interval length to will cause wallpaper to never change"
+	exit 1
+fi
+
+# clear swww cache
+#rm -rf $HOME/.cache/swww
+
+# restart swww
+#swww init
+
+# Edit below to control the images transition
+#export SWWW_TRANSITION_FPS=60
+#export SWWW_TRANSITION_STEP=2
+#export SWWW_TRANSITION=random
+WAL_START=2
+
+# This controls (in seconds) when to switch to the next image
+INTERVAL=$((60*$2))
+
+while true; do
+	find "$1" -type f -name "*.jpg" -o -name "*.png" -o -name "*.gif" \
+		| while read -r img; do
+			echo "$((RANDOM % 1000)):$img"
+		done \
+		| sort -n | cut -d':' -f2- \
+		| while read -r img; do
+			swww img "$img"
+            cp "$img" ~/.cache/current_wallpaper
+            sleep $WAL_START 
+            ~/.config/wal/wal-set.sh "$img"
+            sleep $INTERVAL
+		done
+done
