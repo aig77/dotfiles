587e246472d48e84e5bb87886a0ce929c47f5542
aig77 <29128253+aig77@users.noreply.github.com>
2024-07-05 17:17:17 -0400

    smaller bubble; pacman day bug correction; move clock further right

--- .config/waybar/scripts/check_pacman_updates.sh
+++ .config/waybar/scripts/check_pacman_updates.sh
@@ -7,6 +7,10 @@ last_upgrade=$(tac /var/log/pacman.log |
     cut -d ' ' -f 1 | tr -d '[]')
 t1=$(date +%s -d $last_upgrade)
 t2=$(date +%s) 
-days_since_last_upgrade="$(((t2 - t1) / 86400)) days since last upgrade"
+days_since_last_upgrade=$(( (t2 - t1) / 86400 ))
 
-echo "{\"text\": \"󰇚 ${num_updates}\", \"tooltip\": \"${days_since_last_upgrade}\"}"
+if [ $days_since_last_upgrade -eq 1 ]; then
+    echo "{\"text\": \"󰇚 ${num_updates}\", \"tooltip\": \"${days_since_last_upgrade} day since last upgrade\"}"
+else
+    echo "{\"text\": \"󰇚 ${num_updates}\", \"tooltip\": \"${days_since_last_upgrade} days since last upgrade\"}"
+fi
