5ab7a9ca7253ce6f1f0ab5cd14e1bb8a83f8cf5a
aig77 <29128253+aig77@users.noreply.github.com>
2024-07-08 08:25:12 -0400

    add timer param

--- .config/swaylock/scripts/lockscreentime.sh
+++ .config/swaylock/scripts/lockscreentime.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
+if ! [[ $1 =~ ^-?[0-9]+$ ]]; then
+    echo "Error: script takes one parameter that must be a non-negative integer representing the lock time in minutes"
+    exit 1
+fi
+     
 if [ -f "/usr/bin/swayidle" ]; then
     echo "swayidle is installed."
     swayidle -w \
-        timeout $((60*10)) 'swaylock -f' \
-        timeout $((60*15)) 'hyprctl dispatch dpms off' \
+        timeout $((60*$1)) 'swaylock -f' \
+        timeout $((60 * (5 + $1))) 'hyprctl dispatch dpms off' \
         resume 'hyprctl dispatch dpms on'
 else
     echo "swayidle not installed"
